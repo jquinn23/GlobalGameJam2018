a707b1981d93d649184d97555e2559873bd1bd0c
Mist/Jonathan <quinnjonathan402@gmail.com>
2018-01-27 16:51:22 -0500

    Half finished game controller
    see name

---
+++ Global Game Jam 2018/Assets/Scripts/GameControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control;
+    public int[] 
+    public ArrayList(bool) LevelComplete
+	// Use this for initialization
+	void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if(control != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
