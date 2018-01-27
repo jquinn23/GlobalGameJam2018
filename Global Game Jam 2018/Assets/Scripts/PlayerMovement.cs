b7ce10a48e13e10446bc21052cd55377d530e24a
Mist/Jonathan <quinnjonathan402@gmail.com>
2018-01-26 21:49:47 -0500

    Horizontal Movement
    I've gotten Horizontal Movement running on the Movement branch

---
+++ Global Game Jam 2018/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿/**
+ * @Author Jonathan R. Quinn
+ * <summary>
+ * This script will allow for basic player platforming movement
+ * </summary>
+**/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public Vector3 movementOutput;
+    public Vector3 velocityOutput;
+    private Rigidbody2D playerRigidBody;
+
+
+	// Use this for initialization
+	void Start () {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        
+        //Player shouldn't be able to freely move vertically, so the vertical axis is set to 0
+        Vector3 movement = new Vector3(moveHorizontal, 0);
+        Vector3 temp = playerRigidBody.velocity;
+        temp.x = moveHorizontal * speed;
+        playerRigidBody.velocity = temp;
+        velocityOutput = playerRigidBody.velocity;
+        //playerRigidBody.MovePosition(transform.position +  * Time.deltaTime);
+        movementOutput = movement;
+	}
+}
