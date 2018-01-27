616687daed2a2ae574368b4fc3c720361c5dfc03
ShainaGlass <32582433+ShainaGlass@users.noreply.github.com>
2018-01-27 17:46:28 -0500

    Added end of level doors
    Created a prefab door and created the script to exit scene

---
+++ Global Game Jam 2018/Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour {
+    public GameObject gameManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            gameManager.GetComponent<GameControl>().EndLevel();
+        }
+    }
+
+}
