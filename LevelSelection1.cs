8cfb625f563c640cee96c09113f6b41e97e5a460
rwatkin123 <32782312+rwatkin123@users.noreply.github.com>
2018-03-05 01:17:50 -0500

    Add files via upload

---
+++ LevelSelection1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection1 : MonoBehaviour {
+
+	public void LoadLevel(string levelName)
+	{
+
+		SceneManager.LoadScene (levelName);
+		
+	}
+}
